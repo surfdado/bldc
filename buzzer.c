78027aa1a51c08febe4d96f145a18daba91f324f
Dado Mista <dadomista@gmail.com>
2020-12-08 08:40:39 -0800

    Buzzer: generic infrastructure for beeper/buzzer alerts
    Signed-off-by: Dado Mista <dadomista@gmail.com>

---
+++ buzzer.c
@@ -0,0 +1,73 @@
+#include "buzzer.h"
+
+#ifdef HAS_EXT_BUZZER
+// TODO: Make this configurable from the app
+#define ALERT_MIN_BEEP_MS 1200
+
+#define BEEP_SHORT 0
+#define BEEP_LONG 1
+
+static int alert_beep_num_left = 0;
+static systime_t alert_beep_time;
+static unsigned int alert_beep_duration = BEEP_SHORT;
+static bool is_enabled = true;
+
+void buzzer_enable(bool enable) {
+	is_enabled = enable;
+	if (!enable) {
+		EXT_BUZZER_OFF();
+	}
+}
+bool is_buzzer_enabled() {
+	return is_enabled;
+}
+
+void check_beep_alert(void)
+{
+	if (!is_enabled)
+		return;
+	if (alert_beep_num_left > 0) {
+		if (chVTGetSystemTimeX() - alert_beep_time > alert_beep_duration) {
+			alert_beep_time = chVTGetSystemTimeX();
+			alert_beep_num_left--;
+
+			if (alert_beep_num_left & 0x1)
+				EXT_BUZZER_ON();
+			else
+				EXT_BUZZER_OFF();
+		}
+	}
+}
+
+void beep_alert(int num_beeps, bool longbeep)
+{
+	if (!is_enabled)
+		return;
+	if (alert_beep_num_left == 0) {
+		alert_beep_num_left = num_beeps * 2;
+		alert_beep_time = chVTGetSystemTimeX();
+		alert_beep_duration = longbeep ? 4 * ALERT_MIN_BEEP_MS : ALERT_MIN_BEEP_MS;
+		EXT_BUZZER_ON();
+	}
+}
+
+void beep_off(bool force)
+{
+	// don't mess with the buzzer if we're in the process of doing a multi-beep
+	if (force || (alert_beep_num_left == 0))
+		EXT_BUZZER_OFF();
+}
+
+void beep_on(bool force)
+{
+	if (!is_enabled)
+		return;
+	// don't mess with the buzzer if we're in the process of doing a multi-beep
+	if (force || (alert_beep_num_left == 0))
+		EXT_BUZZER_ON();
+}
+
+#else
+#define check_beep_alert(void) {}
+#define beep_alert(int, bool) {}
+#endif
