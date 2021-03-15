069a22c618580fe25aa61c2383116e36449172f2
Dado Mista <dadomista@gmail.com>
2021-03-14 21:09:21 -0700

    Build Little FOCer instead of CFOC2
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- conf_general.h
+++ conf_general.h
@@ -155,8 +155,8 @@
 //#define HW_SOURCE "hw_stormcore_100s.c"
 //#define HW_HEADER "hw_stormcore_100s.h"
 
-#define HW_SOURCE "hw_Cheap_FOCer_2.c"
-#define HW_HEADER "hw_Cheap_FOCer_2.h"
+//#define HW_SOURCE "hw_Cheap_FOCer_2.c"
+//#define HW_HEADER "hw_Cheap_FOCer_2.h"
 
 //#define HW_SOURCE "hw_140_300.c"
 //#define HW_HEADER "hw_140_300.h"
@@ -164,8 +164,8 @@
 //#define HW_SOURCE "hw_es19.c"
 //#define HW_HEADER "hw_es19.h"
 
-//#define HW_SOURCE "hw_Little_FOCer.c"
-//#define HW_HEADER "hw_Little_FOCer.h"
+#define HW_SOURCE "hw_Little_FOCer.c"
+#define HW_HEADER "hw_Little_FOCer.h"
 
 //#define HW_SOURCE "hw_100_500.c"
 //#define HW_HEADER "hw_100_500.h"
