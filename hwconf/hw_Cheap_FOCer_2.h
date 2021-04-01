15bd6bf4778d7a244331a6e83d97838d63e42edf
Dado Mista <dadomista@gmail.com>
2021-04-01 05:49:05 -0700

    Cheap FOCer v0.9: add "+" to HW String
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- hwconf/hw_Cheap_FOCer_2.h
+++ hwconf/hw_Cheap_FOCer_2.h
@@ -20,7 +20,7 @@
 #ifndef HW_Cheap_FOCer_2
 #define HW_Cheap_FOCer_2
 
-#define HW_NAME					"Cheap_FOCer_2_v0.9"
+#define HW_NAME					"Cheap_FOCer_2_v0.9+"
 
 // HW properties
 #define HW_HAS_DRV8301
