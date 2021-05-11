3b45237a540fdb067b27c7873e3e2e4179a8db88
Dado Mista <dadomista@gmail.com>
2021-05-11 16:26:07 -0700

    HACK: Reduce/Eliminate OW tiltback oscillations
    Hardcoded stepsize / 3 for tiltback recoil
    
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- applications/app_balance.c
+++ applications/app_balance.c
@@ -379,7 +379,7 @@ void calculate_setpoint_interpolated(void){
 		}else if (setpoint_target - setpoint_target_interpolated > 0){
 			setpoint_target_interpolated += get_setpoint_adjustment_step_size();
 		}else{
-			setpoint_target_interpolated -= get_setpoint_adjustment_step_size();
+			setpoint_target_interpolated -= get_setpoint_adjustment_step_size() / 3;
 		}
 	}
 }
