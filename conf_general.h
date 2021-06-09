ad721de3a5a05115f079f2cec2a908dbd72ae5df
Dado Mista <dadomista@gmail.com>
2021-06-08 19:25:38 -0700

    Build HW60 instead of LFOC
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- conf_general.h
+++ conf_general.h
@@ -72,10 +72,10 @@
 // Mark3 version of HW60 with power switch and separate NRF UART.
 //#define HW60_IS_MK3
 //#define HW60_IS_MK4
-#define HW60_IS_MK5
+//#define HW60_IS_MK5
 
-//#define HW_SOURCE "hw_60.c"
-//#define HW_HEADER "hw_60.h"
+#define HW_SOURCE "hw_60.c"
+#define HW_HEADER "hw_60.h"
 
 //#define HW_SOURCE "hw_r2.c"
 //#define HW_HEADER "hw_r2.h"
@@ -164,8 +164,8 @@
 //#define HW_SOURCE "hw_es19.c"
 //#define HW_HEADER "hw_es19.h"
 
-#define HW_SOURCE "hw_Little_FOCer.c"
-#define HW_HEADER "hw_Little_FOCer.h"
+//#define HW_SOURCE "hw_Little_FOCer.c"
+//#define HW_HEADER "hw_Little_FOCer.h"
 
 //#define HW_SOURCE "hw_100_500.c"
 //#define HW_HEADER "hw_100_500.h"
