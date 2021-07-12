ba3b53834be83ace6a66a6f113bbb1ee4a76e417
Dado Mista <dadomista@gmail.com>
2021-07-11 21:39:06 -0700

    Build Little FOCer instead of HW60
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- conf_general.h
+++ conf_general.h
@@ -74,8 +74,8 @@
 //#define HW60_IS_MK4
 #define HW60_IS_MK5
 
-#define HW_SOURCE "hw_60.c"
-#define HW_HEADER "hw_60.h"
+//#define HW_SOURCE "hw_60.c"
+//#define HW_HEADER "hw_60.h"
 
 //#define HW_SOURCE "hw_r2.c"
 //#define HW_HEADER "hw_r2.h"
@@ -162,8 +162,8 @@
 //#define HW_SOURCE "hw_es19.c"
 //#define HW_HEADER "hw_es19.h"
 
-//#define HW_SOURCE "hw_Little_FOCer.c"
-//#define HW_HEADER "hw_Little_FOCer.h"
+#define HW_SOURCE "hw_Little_FOCer.c"
+#define HW_HEADER "hw_Little_FOCer.h"
 
 //#define HW_SOURCE "hw_100_500.c"
 //#define HW_HEADER "hw_100_500.h"
