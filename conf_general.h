b38c40579a9784cf6d3f8a31bc846522c43402cd
Tharon Rivera <>
2021-09-22 22:04:18 -0400

    Support for Spintend ubox single

--- conf_general.h
+++ conf_general.h
@@ -74,8 +74,8 @@
 //#define HW60_IS_MK4
 //#define HW60_IS_MK5
 
-#define HW_SOURCE "hw_60.c"
-#define HW_HEADER "hw_60.h"
+//#define HW_SOURCE "hw_60.c"
+//#define HW_HEADER "hw_60.h"
 
 //#define HW_SOURCE "hw_r2.c"
 //#define HW_HEADER "hw_r2.h"
@@ -183,6 +183,10 @@
 
 //#define HW_SOURCE "hw_raiden7.c"
 //#define HW_HEADER "hw_raiden7.h"
+
+#define HW_SOURCE "hw_ubox_single.c"
+#define HW_HEADER "hw_ubox_single.h"
+
 #endif
 
 #ifndef HW_SOURCE
