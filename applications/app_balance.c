37a3c78b98e0bedb82ec7a5b6cf4fd41a702a6a0
Dado Mista <dadomista@gmail.com>
2022-09-26 09:37:16 -0700

    V6
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- applications/app_balance.c
+++ applications/app_balance.c
@@ -36,7 +36,7 @@
 #include <math.h>
 #include <stdio.h>
 
-#define FW_REVISION 5
+#define FW_REVISION 6
 
 // Data type (Value 5 was removed, and can be reused at a later date, but i wanted to preserve the current value's numbers for UIs)
 typedef enum {
