d691a09a99243a261742175500e463f6bd01bec8
Dado Mista <dadomista@gmail.com>
2024-04-07 09:02:07 -0700

    Thor: robust shutdown handling based on rising edge and hold
    shutdown can now only be triggered by a long-press
    -500ms of button press triggers shutdown provided erpm < 100
    -above 100 erpm the button must be held for 3 seconds to force shutdown
    
    actual shutdown happens on release of the button
    
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- hwconf/fungineers/hw_Thor300_core.c
+++ hwconf/fungineers/hw_Thor300_core.c
@@ -26,12 +26,17 @@
 #include "terminal.h"
 #include "commands.h"
 #include "mc_interface.h"
+#include "buzzer.h"
 
 // Variables
 static volatile bool i2c_running = false;
 static mutex_t shutdown_mutex;
 static float bt_diff = 0.0;
-
+static float bt_lastval = 0.0;
+static float bt_unpressed = 0.0;
+static bool will_poweroff = false;
+static bool force_poweroff = false;
+static unsigned int bt_hold_counter = 0;
 
 // I2C configuration
 static const I2CConfig i2cfg = {
@@ -253,24 +258,121 @@ void hw_try_restore_i2c(void) {
 	}
 }
 
+#define RISING_EDGE_THRESHOLD 0.09
+#define TIME_500MS 50
+#define TIME_3S 300
+#define ERPM_THRESHOLD 100
 
+/**
+ * hw_sample_shutdown_button - return false if shutdown is requested, true otherwise
+ *
+ * Behavior: after determining the unpressed level, look for rising edges or values
+ * that are clearly above the unpressed level (2 x Threshold higher), triggering a counter.
+ *
+ * Once triggered, the counter keeps incrementing as long as the level is 2 x Threshold higher
+ * than the normal/unpressed value, otherwise it gets reset to zero.
+ *
+ * Once the counter reaches the threshold the button is considered pressed, provided that
+ * the erpm is below 100. A very short (20ms) beep will go off.
+ * Shutdown actually happens on the falling edge when the press is over.
+ *
+ * If the motor is spinning faster, then a 3s press is required. Buzzer will beep once the
+ * time has been reached. Again, shutdown happens on the falling edge.
+ *
+ * Normal shutdown time:    0.5s
+ * Emergency shutdown time: 3.0s
+ */
 
 bool hw_sample_shutdown_button(void) {
-	#ifdef ALWAYS_ON
-	return true;
-    #endif
+    chMtxLock(&shutdown_mutex);
+    float newval = ADC_VOLTS(ADC_IND_SHUTDOWN);
+    chMtxUnlock(&shutdown_mutex);
+    if (bt_lastval == 0) {
+        bt_lastval = newval;
+    }
+    bt_diff = (newval - bt_lastval);
 
-	bt_diff = 0.0;
-	chMtxLock(&shutdown_mutex);
-	float val1 =ADC_VOLTS(ADC_IND_SHUTDOWN);
-	chThdSleepMilliseconds(5);
-	float val2 = ADC_VOLTS(ADC_IND_SHUTDOWN);
-	chMtxUnlock(&shutdown_mutex);
-	bt_diff += (val1 - val2);
+    bool is_steady = fabsf(bt_diff) < 0.02;  // filter out noise above 20mV
+    bool is_rising_edge = (bt_diff > RISING_EDGE_THRESHOLD);
 
-	return ((bt_diff < 0.07));
+    bt_lastval = newval;
 
-	}
+    if (bt_unpressed == 0.0) {
+        // initializing bt_unpressed
+        if (is_steady) {
+            bt_unpressed = newval;
+        }
+        // return true regardless (this happens only after boot)
+        return true;
+    }
+
+    if (will_poweroff) {
+        if (!force_poweroff && (fabsf(mc_interface_get_rpm()) > ERPM_THRESHOLD)) {
+            will_poweroff = false;
+            bt_hold_counter = 0;
+            beep_off();
+            return true;
+        }
+
+        // Now we look for a falling edge to shut down
+        if ((bt_diff < -RISING_EDGE_THRESHOLD) || (newval < bt_unpressed + RISING_EDGE_THRESHOLD / 2)) {
+            bt_hold_counter++;
+            beep_off();
+            return false;
+        }
+        return true;
+    }
+
+    if (bt_hold_counter == 0) {
+        if (is_rising_edge || (newval > bt_unpressed + RISING_EDGE_THRESHOLD)) {
+            // trigger by edge and by level!
+            bt_hold_counter = 1;
+        }
+        else {
+            if (is_steady && (newval < bt_unpressed + RISING_EDGE_THRESHOLD / 2)) {
+                // pickup drifts due to temperature
+                bt_unpressed = bt_unpressed * 0.9 + newval * 0.1;
+            }
+        }
+    }
+    else {
+        // we've had a rising edge and are now checking for a steady hold
+        if (newval > bt_unpressed + RISING_EDGE_THRESHOLD * 1.5) {
+            bt_hold_counter++;
+            //LED_RED_ON();
+
+            if (bt_hold_counter > TIME_500MS) {
+                if (fabsf(mc_interface_get_rpm()) < ERPM_THRESHOLD) {
+                    // after 150ms, power-down is triggered by the falling edge (releasing the button)
+                    will_poweroff = true;
+                    bt_hold_counter = 0;
+
+                    // super short beep to let the user know they can let go of the button now
+                    beep_on();
+                    chThdSleepMilliseconds(20);
+                    beep_off();
+                }
+                else {
+                    if (bt_hold_counter  > TIME_3S) {
+                        // Emergency Power-Down - beep to let the user know it's ready
+                        beep_on();
+                        will_poweroff = true;
+                        force_poweroff = true;
+                        bt_hold_counter = 0;
+                        return true;
+                    }
+                }
+            }
+        }
+        else {
+            // press is too short, abort
+            bt_hold_counter = 0;
+            beep_off();
+            //LED_RED_OFF();
+        }
+    }
+    return true;
+}
 
 
 float hw_Thor_get_temp(void) {
@@ -291,7 +393,8 @@ static void terminal_button_test(int argc, const char **argv) {
 	(void)argv;
 
 	for (int i = 0;i < 40;i++) {
-		commands_printf("BT: %d %.2f", HW_SAMPLE_SHUTDOWN(), (double)bt_diff);
+		commands_printf("BT: %d:%d [%.2fV], %.2fV, %.2fV, OFF=%d", HW_SAMPLE_SHUTDOWN(), bt_hold_counter,
+                        (double)bt_diff, (double)bt_unpressed, (double)bt_lastval, (int)will_poweroff);
 		chThdSleepMilliseconds(100);
 	}
 }
